337e2f60173c6d151484d37a9afce8e164aed468
aleenaloves <aleena@bocoup.com>
2023-04-13 11:54:10 -0500

    page styling

--- page.php
+++ page.php
@@ -1,8 +1,8 @@
-<?php get_header()?>
+<?php get_header() ?>
 
 <div class="page-content">
-<?php echo the_title(); ?>
+<h1><?php echo the_title(); ?></h1>
 <?php the_content() ?>
 </div>
 
-<?php get_footer()?>
+<?php get_footer() ?>
