4baa4b5205bf58e9a2009600542cd1572e30e219
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2023-04-24 12:50:24 -0700

    Adjusts page styling to mimic case study layout

--- page.php
+++ page.php
@@ -1,8 +1,22 @@
 <?php get_header() ?>
-
-<div class="page-content">
-<h1><?php echo the_title(); ?></h1>
-<?php the_content() ?>
+<div class="breadcrumb-container">
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+            <li><a href="<?php echo get_home_url() ?>">Home</a></li>
+            <li>
+              <a href="<?php esc_url(the_permalink()) ?>" aria-current="page">
+                    <?php echo get_the_title(); ?>
+                </a>
+            </li>
+        </ul>
+    </nav>
 </div>
+
+<main class="hero page-content">
+  <div class="page-content-body hero-body">
+    <h1><?php echo the_title(); ?></h1>
+    <?php the_content() ?>
+  </div>
+</main>
 
 <?php get_footer() ?>
